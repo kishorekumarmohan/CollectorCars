45e0bb6a5f2ea918d0846913c81e2e8322cb48fb
Mohan <kmohan@ebay.com>
2015-08-13 23:50:00 -0700

    expand collapse animations when tapping on table row.

--- CollectorCars/KKMCollectorCarsSettingsViewController.m
+++ CollectorCars/KKMCollectorCarsSettingsViewController.m
@@ -14,6 +14,9 @@ NSInteger const KKMPriceIncrement = 50000;
 NSInteger const KKMTableRowExpandHeight = 200.0f;
 NSInteger const KKMTableRowDefaultHeight = 44.0f;
 
+NSString* const KKMNoMinString = @"No Min";
+NSString* const KKMNoMaxString = @"No Max";
+
 @interface KKMCollectorCarsSettingsViewController () <UITableViewDelegate, UITableViewDataSource, UIPickerViewDataSource, UIPickerViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIPickerView *yearFromPickerView;
@@ -24,10 +27,13 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
 @property (weak, nonatomic) IBOutlet UIPickerView *priceMaxPickerView;
 @property (weak, nonatomic) IBOutlet UILabel *priceSelectedValueLabel;
 
-@property (nonatomic, strong) NSIndexPath *selectedIndexPath;
+@property (nonatomic, strong) NSMutableArray *yearFromRangeArray;
+@property (nonatomic, strong) NSMutableArray *yearToRangeArray;
 
-@property (nonatomic, strong) NSMutableArray *yearRangeArray;
-@property (nonatomic, strong) NSMutableArray *priceRangeArray;
+@property (nonatomic, strong) NSMutableArray *priceMinRangeArray;
+@property (nonatomic, strong) NSMutableArray *priceMaxRangeArray;
+
+@property (nonatomic, strong) NSIndexPath *selectedIndexPath;
 
 @end
 
@@ -45,8 +51,8 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
 {
     [super viewDidAppear:animated];
     
-    [self.yearFromPickerView selectRow:54 inComponent:0 animated:YES];
-    [self.yearToPickerView selectRow:(self.yearRangeArray.count - 1) inComponent:0 animated:YES];
+    [self.yearToPickerView selectRow:(self.yearToRangeArray.count - 1) inComponent:0 animated:YES];
+    [self.priceMaxPickerView selectRow:(self.priceMaxRangeArray.count -1) inComponent:0 animated:YES];
 }
 
 - (void)setUp
@@ -60,34 +66,40 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
 - (void)setUpPickerData
 {
     // year
-    self.yearRangeArray = [NSMutableArray new];
-    [self.yearRangeArray addObject:@"Any"];
+    self.yearFromRangeArray = [NSMutableArray new];
+    self.yearToRangeArray = [NSMutableArray new];
+    [self.yearFromRangeArray addObject:KKMNoMinString];
+    
     NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
     NSInteger year = [components year];
 
     for (int i = 1896; i <= year; i++)
     {
-        [self.yearRangeArray addObject:[NSString stringWithFormat:@"%d", i]];
+        [self.yearFromRangeArray addObject:[NSString stringWithFormat:@"%d", i]];
+        [self.yearToRangeArray addObject:[NSString stringWithFormat:@"%d", i]];
     }
-    [self.yearRangeArray addObject:@"To"];
-    
-    
-    
+    [self.yearToRangeArray addObject:KKMNoMaxString];
     
     // price
-    self.priceRangeArray = [NSMutableArray new];
-    [self.priceRangeArray addObject:@"Any"];
+    self.priceMinRangeArray = [NSMutableArray new];
+    self.priceMaxRangeArray = [NSMutableArray new];
+    [self.priceMinRangeArray addObject:KKMNoMinString];
 
     NSInteger j = 0;
     NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
     [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
     [numberFormatter setMaximumFractionDigits:0];
 
+    NSString *str;
     while (j <= KKMMaxPriceRange)
     {
         j += KKMPriceIncrement;
-        [self.priceRangeArray addObject:[numberFormatter stringFromNumber:[NSNumber numberWithInteger:j]]];
-    }    
+        str =[numberFormatter stringFromNumber:[NSNumber numberWithInteger:j]];
+        [self.priceMinRangeArray addObject:str];
+        [self.priceMaxRangeArray addObject:str];
+    }
+    
+    [self.priceMaxRangeArray addObject:KKMNoMaxString];
 }
 
 - (IBAction)close:(id)sender
@@ -95,44 +107,65 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-#pragma mark - UITableViewDataSource
-
-
 #pragma mark - UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSArray *viewsToShow;
-    NSArray *viewsToHide;
     
     if (indexPath.row == 0)
         return;
-    else if(indexPath.row == 1)
+
+    NSArray *viewsToShow;
+    NSArray *viewsToHide;
+    
+    if(indexPath.row == 1)
     {
-        viewsToShow = @[self.yearFromPickerView, self.yearToPickerView];
-        viewsToHide = @[self.yearSelectedValueLablel];
+        if (self.selectedIndexPath.row == 1)
+        {
+            viewsToShow = @[self.yearSelectedValueLablel];
+            viewsToHide = @[self.yearFromPickerView, self.yearToPickerView];
+        }
+        else if (self.selectedIndexPath.row == 2)
+        {
+            viewsToShow = @[self.yearFromPickerView, self.yearToPickerView, self.priceSelectedValueLabel];
+            viewsToHide = @[self.priceMinPickerView, self.priceMaxPickerView, self.yearSelectedValueLablel];
+        }
+        else
+        {
+            viewsToShow = @[self.yearFromPickerView, self.yearToPickerView];
+            viewsToHide = @[self.yearSelectedValueLablel];
+        }
     }
     else if(indexPath.row == 2)
     {
-        viewsToShow = @[self.priceMinPickerView, self.priceMaxPickerView];
-        viewsToHide = @[self.priceSelectedValueLabel];
+        if (self.selectedIndexPath.row == 1)
+        {
+            viewsToShow = @[self.priceMinPickerView, self.priceMaxPickerView, self.yearSelectedValueLablel];
+            viewsToHide = @[self.yearFromPickerView, self.yearToPickerView, self.priceSelectedValueLabel];
+        }
+        else if (self.selectedIndexPath.row == 2)
+        {
+            viewsToShow = @[self.priceSelectedValueLabel];
+            viewsToHide = @[self.priceMinPickerView, self.priceMaxPickerView];
+        }
+        else
+        {
+            viewsToShow = @[self.priceMinPickerView, self.priceMaxPickerView];
+            viewsToHide = @[self.priceSelectedValueLabel];
+        }
     }
-
     
-    if (self.selectedIndexPath && [indexPath isEqual:self.selectedIndexPath])
+    if ([indexPath isEqual:self.selectedIndexPath])
     {
         // same row tapped, so collapse the row
         self.selectedIndexPath = nil;
-        [self hideViews:viewsToShow];
-        [self showViews:viewsToHide];
-        [self tableView:tableView collapseRowAtIndexPath:indexPath];
+        [self tableView:tableView collapseRowAtIndexPath:indexPath viewsToShow:viewsToShow viewsToHide:viewsToHide];
         return;
     }
     
     if(self.selectedIndexPath)
     {
-        [self hideViews:viewsToShow];
-        [self showViews:viewsToHide];
-        [self tableView:tableView expandRowAtIndexPath:indexPath viewsToShow:viewsToShow viewsToHide:viewsToHide];
+        [self tableView:tableView expandRowAtIndexPath:indexPath collapseRowAtIndexPath:self.selectedIndexPath viewsToShow:viewsToShow viewsToHide:viewsToHide];
+        self.selectedIndexPath = indexPath;
     }
     else
     {
@@ -145,8 +178,6 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
 
 - (void)tableView:(UITableView *)tableView expandRowAtIndexPath:(NSIndexPath *)indexPath viewsToShow:(NSArray *)viewsToShow viewsToHide:(NSArray *)viewsToHide
 {
-    [self hideViews:viewsToHide];
-    
     [UIView animateWithDuration:0.5 animations:^{
         for (UITableViewCell *cell in tableView.visibleCells) {
             NSIndexPath *cellIndexPath = [tableView indexPathForCell:cell];
@@ -164,6 +195,7 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
         frame.size.height = KKMTableRowExpandHeight;
         cell.frame = frame;
     } completion:^(BOOL finished) {
+        [self hideViews:viewsToHide];
         [self showViews:viewsToShow];
         [tableView reloadData];  // commit final state
     }];
@@ -187,8 +219,10 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
     }
 }
 
-- (void)tableView:(UITableView *)tableView collapseRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)tableView:(UITableView *)tableView collapseRowAtIndexPath:(NSIndexPath *)indexPath viewsToShow:viewsToShow viewsToHide:viewsToHide
 {
+    [self hideViews:viewsToHide];
+    
     [UIView animateWithDuration:0.5 animations:^{
         // move down cells which are below selected one
         for (UITableViewCell *cell in tableView.visibleCells) {
@@ -207,14 +241,58 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
         frame.size.height = KKMTableRowDefaultHeight;
         cell.frame = frame;
     } completion:^(BOOL finished) {
+        [self showViews:viewsToShow];
         [tableView reloadData];  // commit final state
     }];
 }
 
-- (void)tableView:(UITableView *)tableView expandRowAtIndexPath:(NSIndexPath *)indexPath collapseRowAtIndexPath:(NSIndexPath *)collapseIndexPath
+- (void)tableView:(UITableView *)tableView expandRowAtIndexPath:(NSIndexPath *)expandIndexPath collapseRowAtIndexPath:(NSIndexPath *)collapseIndexPath viewsToShow:viewsToShow viewsToHide:viewsToHide
 {
-}
+    [self hideViews:viewsToHide];
 
+    [UIView animateWithDuration:0.5 animations:^{
+        
+        // move down cells which are below selected one
+        for (UITableViewCell *cell in tableView.visibleCells) {
+            NSIndexPath *cellIndexPath = [tableView indexPathForCell:cell];
+            
+            if (cellIndexPath.row > collapseIndexPath.row) {
+                CGRect frame = cell.frame;
+                frame.origin.y = frame.origin.y - KKMTableRowExpandHeight + frame.size.height;
+                cell.frame = frame;
+            }
+        }
+        // expand selected cell
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:collapseIndexPath];
+        
+        CGRect frame = cell.frame;
+        frame.size.height = KKMTableRowDefaultHeight;
+        cell.frame = frame;
+    } completion:^(BOOL finished) {
+        
+        [UIView animateWithDuration:0.5 animations:^{
+            for (UITableViewCell *cell in tableView.visibleCells) {
+                NSIndexPath *cellIndexPath = [tableView indexPathForCell:cell];
+                
+                if (cellIndexPath.row > expandIndexPath.row) {
+                    CGRect frame = cell.frame;
+                    frame.origin.y += KKMTableRowExpandHeight - frame.size.height;
+                    cell.frame = frame;
+                }
+            }
+            // expand selected cell
+            UITableViewCell *cell = [tableView cellForRowAtIndexPath:expandIndexPath];
+            
+            CGRect frame = cell.frame;
+            frame.size.height = KKMTableRowExpandHeight;
+            cell.frame = frame;
+        } completion:^(BOOL finished) {
+            [self showViews:viewsToShow];
+            [tableView reloadData];  // commit final state
+        }];
+        
+    }];
+}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -246,68 +324,74 @@ NSInteger const KKMTableRowDefaultHeight = 44.0f;
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    if([self isYearPicker:pickerView])
-        return self.yearRangeArray.count;
-    else if([self isPricePicker:pickerView])
-        return self.priceRangeArray.count;
+    if([pickerView isEqual:self.yearFromPickerView])
+        return self.yearFromRangeArray.count;
+    else if([pickerView isEqual:self.yearToPickerView])
+        return self.yearToRangeArray.count;
+    else if([pickerView isEqual:self.priceMinPickerView])
+        return self.priceMinRangeArray.count;
+    else if([pickerView isEqual:self.priceMaxPickerView])
+        return self.priceMaxRangeArray.count;
+
     return 0;
 }
-
-- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-{
-    if([self isYearPicker:pickerView])
-        return self.yearRangeArray[row];
-    else if([self isPricePicker:pickerView])
-        return self.priceRangeArray[row];
-    return 0;
-}
-
-- (BOOL)isYearPicker:(UIPickerView *)pickerView
-{
-    return ([pickerView isEqual:self.yearFromPickerView] || [pickerView isEqual:self.yearToPickerView]);
-}
-
-- (BOOL)isPricePicker:(UIPickerView *)pickerView
-{
-    return ([pickerView isEqual:self.priceMinPickerView] || [pickerView isEqual:self.priceMaxPickerView]);
-}
-
 
 - (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
 {
-    if ([self isYearPicker:pickerView])
+    NSString *text;
+    
+    if([pickerView isEqual:self.yearFromPickerView])
+        text = [self.yearFromRangeArray objectAtIndex:row];
+    else if([pickerView isEqual:self.yearToPickerView])
+        text = [self.yearToRangeArray objectAtIndex:row];
+    else if([pickerView isEqual:self.priceMinPickerView])
+        text = [self.priceMinRangeArray objectAtIndex:row];
+    else if([pickerView isEqual:self.priceMaxPickerView])
+        text = [self.priceMaxRangeArray objectAtIndex:row];
+    
+    return [self pickerReusingView:view withText:text];
+}
+
+- (UIView *)pickerReusingView:(UIView *)view withText:(NSString *)text
+{
+    UILabel* tView = (UILabel*)view;
+    if (!tView)
     {
-        UILabel* tView = (UILabel*)view;
-        if (!tView)
-        {
-            tView = [[UILabel alloc] init];
-            [tView setFont:[UIFont systemFontOfSize:12]];
-            [tView setTextAlignment:NSTextAlignmentCenter];
-        }
-        tView.text=[self.yearRangeArray objectAtIndex:row];
-        return tView;
+        tView = [[UILabel alloc] init];
+        [tView setFont:[UIFont systemFontOfSize:12]];
+        [tView setTextAlignment:NSTextAlignmentCenter];
     }
-    else if([self isPricePicker:pickerView])
-    {
-        UILabel* tView = (UILabel*)view;
-        if (!tView)
-        {
-            tView = [[UILabel alloc] init];
-            [tView setFont:[UIFont systemFontOfSize:12]];
-            [tView setTextAlignment:NSTextAlignmentCenter];
-        }
-        tView.text=[self.priceRangeArray objectAtIndex:row];
-        return tView;
-    }
-    return nil;
+    tView.text = text;
+    return tView;
 }
 
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
+    NSInteger yearFromRow = [self.yearFromPickerView selectedRowInComponent:0];
+    NSInteger yearToRow = [self.yearToPickerView selectedRowInComponent:0] + 1;
+    
+    NSInteger priceMinRow = [self.priceMinPickerView selectedRowInComponent:0];
+    NSInteger priceMaxRow = [self.priceMaxPickerView selectedRowInComponent:0];
+
     if ([pickerView isEqual:self.yearFromPickerView])
     {
-        //- (void)selectRow:(NSInteger)row inComponent:(NSInteger)component animated:(BOOL)animated;  // scrolls the specified row to center.
-        [self.yearToPickerView selectRow:row inComponent:0 animated:YES];
+        if (yearFromRow > yearToRow)
+            [self.yearToPickerView selectRow:row-1 inComponent:0 animated:YES];
+    }
+    else if ([pickerView isEqual:self.yearToPickerView])
+    {
+        if (yearToRow < yearFromRow)
+            [self.yearFromPickerView selectRow:yearToRow inComponent:0 animated:YES];
+    }
+    else if ([pickerView isEqual:self.priceMinPickerView])
+    {
+        if (priceMinRow > priceMaxRow)
+            [self.priceMaxPickerView selectRow:priceMinRow inComponent:0 animated:YES];
+    }
+    else if ([pickerView isEqual:self.priceMaxPickerView])
+    {
+        if (priceMaxRow < priceMinRow)
+            [self.priceMinPickerView selectRow:priceMaxRow inComponent:0 animated:YES];
     }
 }
 
